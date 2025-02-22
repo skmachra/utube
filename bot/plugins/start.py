0acfd0bb7a3dd21e048841876517e85d55fcb77b
Sunil Kumar <147473716+skmachra@users.noreply.github.com>
2025-02-22 17:24:29 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -1,5 +1,6 @@
 from pyrogram import filters as Filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram.enums import ChatAction
 
 from ..translations import Messages as tr
 from ..config import Config
@@ -13,7 +14,7 @@ from ..utubebot import UtubeBot
     & Filters.user(Config.AUTH_USERS)
 )
 async def _start(c: UtubeBot, m: Message):
-    await m.reply_chat_action("typing")
+    await m.reply_chat_action(ChatAction.TYPING)
 
     await m.reply_text(
         text=tr.START_MSG.format(m.from_user.first_name),
