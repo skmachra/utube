057d0cf7ffb2e1aa16f50d6d836e7979e72bf06a
Sunil Kumar <147473716+skmachra@users.noreply.github.com>
2025-02-22 17:28:17 +0530

    Update help.py

--- bot/plugins/help.py
+++ bot/plugins/help.py
@@ -1,4 +1,5 @@
 from pyrogram import filters as Filters
+from pyrogram.enums import ChatAction
 from pyrogram.types import (
     InlineKeyboardMarkup,
     InlineKeyboardButton,
@@ -39,7 +40,7 @@ def map_btns(pos):
     & Filters.user(Config.AUTH_USERS)
 )
 async def _help(c: UtubeBot, m: Message):
-    await m.reply_chat_action("typing")
+    await m.reply_chat_action(ChatAction.TYPING)
     await m.reply_text(
         text=tr.HELP_MSG[1],
         reply_markup=InlineKeyboardMarkup(map_btns(1)),
